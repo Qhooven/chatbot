6d2aeae54c139516b6789aec4f188d93a5994a94
Qhooven <55033528+Qhooven@users.noreply.github.com>
2021-07-11 18:05:22 -0400

    Update settings.py

--- Desktop/benderbot/benderbot/benderbot/settings.py
+++ Desktop/benderbot/benderbot/benderbot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-fh03%tj4t*8dt-6tusyru%x=uzm%u)x2_x3q0xvep9udl7n&d'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
